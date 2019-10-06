1653604918c402219b5e3815b334cc130851b69a
Uzair Ahmed <uzairahmed11d@gmail.com>
2019-10-06 13:31:02 +0500

    Create Increasing Triplet Subsequence.java
    Runtime: 0 ms, faster than 100.00% of Java online submissions for Increasing Triplet Subsequence.
    Memory Usage: 38.4 MB, less than 95.35% of Java online submissions for Increasing Triplet Subsequence.

---
+++ Leetcode/Increasing Triplet Subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int i = Integer.MAX_VALUE;
+        int j = Integer.MAX_VALUE;
+        for(int k = 0; k < nums.length; k++)
+        {
+            if(nums[k] <= i)
+                i = nums[k];
+            else if(nums[k] <= j)
+                j = nums[k];
+            else
+                return true;
+        }
+        return false;
+    }
+}
